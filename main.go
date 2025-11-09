4afe0429f8bd9dcdf38624ddfbb574e302544127
Emerson <emerson.day@gmail.com>
2025-11-08 16:00:29 -0800

    simple db

--- main.go
+++ main.go
@@ -6,10 +6,29 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, world")
-	})
+type Datastore interface{ Save(data string) error }
+type Server struct{ db Datastore }
+type SimpleDB struct{}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func (db *SimpleDB) Save(data string) error {
+	log.Printf("REAL_DB: Saving data: %s", data)
+
+	return nil
+}
+func main() {
+	realDB := &SimpleDB{}
+
+	server := &Server{db: realDB}
+
+	http.HandleFunc("/hello", server.HelloHandler)
+}
+
+func (s *Server) HelloHandler(w http.ResponseWriter, r *http.Request) {
+	err := s.db.Save("hello, world")
+	if err != nil {
+		http.Error(w, "Could not save data", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Data saved: hello, world")
 }
