5faf2ea44ba656dcf3562a9feeb856496ad12c5e
Emerson <emerson.day@gmail.com>
2025-11-09 14:47:22 -0800

    test params

--- main.go
+++ main.go
@@ -6,31 +6,19 @@ import (
 	"net/http"
 )
 
-type Datastore interface{ Save(data string) error }
-type Server struct{ db Datastore }
-type SimpleDB struct{}
-
-func (db *SimpleDB) Save(data string) error {
-	log.Print("saving")
-	return nil
-}
-
 func main() {
-	theDb := &SimpleDB{}
-	server := &Server{db: theDb}
-
-	http.HandleFunc("/hello", server.HelloHandler)
 
 	log.Println("Starting server on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func (s *Server) HelloHandler(w http.ResponseWriter, r *http.Request) {
-	err := s.db.Save("hello, world")
-	if err != nil {
-		http.Error(w, "Could not save data", http.StatusInternalServerError)
+func GetCommentHandler(w http.ResponseWriter, r *http.Request) {
+	videoId := r.URL.Query().Get("video_id")
+	if videoId == "" {
+		http.Error(w, "no video id param", http.StatusBadRequest)
 		return
 	}
 
-	fmt.Fprintf(w, "Data saved: hello, world")
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Fetching video with id: %v", videoId)
 }
--- main_test.go
+++ main_test.go
@@ -3,22 +3,26 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestHelloHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/hello", nil)
+func TestGetCommentHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/comment?video_id=3", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(HelloHandler)
+	handler := http.HandlerFunc(GetCommentHandler)
 
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		t.Errorf("wrong status: %v (not 200)", status)
 	}
 
+	if body := rr.Body.String(); !strings.Contains(body, "v123") {
+		t.Errorf("body doesnt contain v123")
+	}
 }
